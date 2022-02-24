c96aa30a9ca50281108a15a9da405f322b02edea
Nicoll Oliver <nicolldon96@gmail.com>
2022-02-23 19:46:43 -0500

    object literal example file add

---
+++ obj_literals.js
@@ -0,0 +1,14 @@
+// object literals
+    // use curly braces to make an obj literal (key/value pairs) - example: 
+
+let user = {
+    name: 'Topanga', // This makes up one property of this object literal 
+    age: 30, // each key/value pair is on each line for better readability purposes
+    email: 'toptest@test.com',
+    location: 'Brazil',
+    blogs: ['Why Panera mac and cheese is the best', '10 things to know when...']
+};
+
+// test it out:
+
+console.log(user);
