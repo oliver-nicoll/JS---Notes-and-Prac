5df32deff2a066f55b7fd877558db9eb149094d0
Nicoll Oliver <nicolldon96@gmail.com>
2022-02-23 20:08:29 -0500

    dot notation vs square bracket notation examples

--- obj_literals.js
+++ obj_literals.js
@@ -9,6 +9,30 @@ let user = {
     blogs: ['Why Panera mac and cheese is the best', '10 things to know when...']
 };
 
-// test it out:
+// test it out, either run "node FILENAME.js" in terminal or open FILENAME.html that is connected to the file:
 
 console.log(user);
+
+// to access a specific property or key/value pair from the object, use dot notation
+
+console.log(user.blogs)
+
+//to change a specific property of the object LATER in the code:
+
+user.age = 32;
+
+//check if the age was changed
+
+console.log(user.age)
+
+
+//ANOTHER way to access properties by using square bracket notation
+
+console.log(user['name'])
+
+console.log(user['blogs'][1]) //accessing the second blog title specifically
+
+// to change a property using this square bracket notation: 
+user['location'] = 'Peru'
+//check to see if changed
+console.log(user['location'])
